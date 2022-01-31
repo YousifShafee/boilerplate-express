280a56cab7fbe4db3b63b383ee87f43edd4074b9
Yousif Shafee <yousiftariq1@hotmail.com>
2022-01-31 02:53:56 +0200

    Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
+
+function mware(req, res, next) {
+  console.log(req.method + " " + req.path + ' - ' + req.ip)
+  next()
+}
+app.use(mware)
+
 const hand = function(req, res){
     let path = __dirname + "/views/index.html";
     res.sendFile(path);
 }
+
 app.use('/public', express.static(__dirname + "/public"))
+
 console.log(app.get('/', hand))
+
 const json_fun = function(req, res){
   if(process.env.MESSAGE_STYLE == 'uppercase'){
     res.json({"message": "Hello json".toUpperCase()})
@@ -16,25 +26,6 @@ const json_fun = function(req, res){
   }  
 }
 console.log(app.get('/json', json_fun))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
